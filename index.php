c2ca89f596e61f9bffdfb72b4f89569e2f4043ca
Craig Blunden <craig.blunde89@gmail.com>
2012-11-26 01:26:34 +1000

    new index

---
+++ index.php
@@ -0,0 +1,36 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!--Next comes the beginning html tag-->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	 <link href="bootstrap.css" rel="stylesheet" type="text/css"/>
+	 
+	 <title>Bulk Bill Me</title>
+
+
+</head>
+<body>
+
+<div class="navbar navbar-inverse">
+  <div class="navbar-inner">
+    <a class="brand" href="index1.php">BulkBillMe</a>
+    <ul class="nav">
+      <li class="active"><a href="index1.php">Home</a></li>
+      <li><a href="about.php">About Us</a></li>
+      <li><a href="search.php">Search for a GP</a></li>
+      <li><a href="add.php">Add a GP</a></li>
+    </ul>
+  </div>
+</div>
+
+<div class="container">
+
+<h1> Welcome to BulkBillMe!</h1><br/><br/>
+
+
+<h2> We are making a beautiful website to improve access to healthcare.  Come and join us.</h2>
+</div>
+</body>
+</html>
