77bd4b3eb6888b0b582d674311cfa6bbc58dd365
cblun1 <craig.blunden89@gmail.com>
2012-11-26 01:29:40 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!--Next comes the beginning html tag-->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -15,7 +14,7 @@
 
 <div class="navbar navbar-inverse">
   <div class="navbar-inner">
-    <a class="brand" href="index1.php">BulkBillMe</a>
+    <a class="brand" href="index.php">BulkBillMe</a>
     <ul class="nav">
       <li class="active"><a href="index1.php">Home</a></li>
       <li><a href="about.php">About Us</a></li>
